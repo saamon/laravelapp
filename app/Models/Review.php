af03e2ff054470e00e34610437e61a0f5552cf1e
suzuki kento <suzuken2960@gmail.com>
2026-05-10 20:39:21 +0900

    refactor: 不要なHasFactoryの使用を削除

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,16 +2,11 @@
 
 namespace App\Models;
 
-use Database\Factories\ReviewFactory;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
-    /** @use HasFactory<ReviewFactory> */
-    use HasFactory;
-
     protected $fillable = [
         'book_id',
         'status',
