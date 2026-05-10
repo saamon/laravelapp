19335e400a5b69179102f991e3b5b7bfcddbbfec
suzuki kento <suzuken2960@gmail.com>
2026-05-10 12:27:21 +0900

    feat: pestのテストコード確認

---
+++ tests/Unit/CalculatorTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Support;
+
+
+use App\Support\Calculator;
+
+test('sum', function () {
+    $calculator = new Calculator();
+    expect($calculator->add(1, 2))->toBe(3);
+});
