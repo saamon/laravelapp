d1b721df07f32a735cdd52632743888e57afbcae
saamon <81011337+saamon@users.noreply.github.com>
2026-06-14 11:30:41 +0000

    Apply automatic changes

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -14,6 +14,7 @@ class BookController extends Controller
     public function index()
     {
         $books = Book::withIndexRelations()->get();
+
         return view('books.index', [
             'books' => $books,
         ]);
